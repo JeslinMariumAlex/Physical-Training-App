2d52c79c06d5f43aabf36eae951d65eff5bda6db
Jeslin Marium Alex <jeslinmariumalex@gmail.com>
2024-12-19 18:20:58 +0530

    url working of main app

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('appointment/', views.appointment, name='appointment'),
+    path('trainers/', views.trainers, name='trainers'),
+    path('contact/', views.contact, name='contact'),
 ]
--- main/views.py
+++ main/views.py
@@ -2,5 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello, World!')
+    return HttpResponse('Home page')
+
+def about(request):
+    return HttpResponse('About page')
+
+def appointment(request):
+    return HttpResponse('Appointment page')
+
+def trainers(request):
+    return HttpResponse('trainers page')
+
+def contact(request):
+    return HttpResponse('contact page')
 
