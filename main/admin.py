664c682d6344a53a04cfc419a67ee601b38ec636
Jeslin Marium Alex <jeslinmariumalex@gmail.com>
2024-12-22 14:04:53 +0530

    appointment model

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Workout, Trainer
+from .models import Workout, Trainer, Appointment
 
 # Register your models here.
 
 admin.site.register(Workout)
 admin.site.register(Trainer)
+admin.site.register(Appointment)
 
--- main/models.py
+++ main/models.py
@@ -16,5 +16,17 @@ class Trainer(models.Model):
     trainer_image = models.ImageField(upload_to='trainer_images')
 
     def __str__(self):
-        return self.trainer_name
+        return self.trainer_name + ' - ' + self.trainer_spec
+
+class Appointment(models.Model):
+    person_name = models.CharField(max_length=100)
+    person_phone = models.CharField(max_length=10)
+    person_email = models.EmailField()
+    trainer_name = models.ForeignKey(Trainer, on_delete=models.CASCADE)
+    appointment_date = models.DateField()
+    appointment_time = models.TimeField()
+    booked_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.person_name
     
