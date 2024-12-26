7ca975c935b6e0a886033eac32ed71f83b71dfd8
Jeslin Marium Alex <jeslinmariumalex@gmail.com>
2024-12-26 14:46:30 +0530

    login required used for accessing website

--- main/views.py
+++ main/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 from .models import Workout, Trainer
 from .forms import AppointmentForm
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
+@login_required
 def index(request):
     return render(request, 'index.html')
 
+@login_required
 def about(request):
     return render(request, 'about.html')
 
+@login_required
 def appointment(request):
     if request.method == 'POST':
         form = AppointmentForm(request.POST)
@@ -22,18 +26,21 @@ def appointment(request):
     }
     return render(request, 'appointment.html', dict_form)
 
+@login_required
 def trainers(request):
     dict_trainers = {
         'trainers': Trainer.objects.all()
     }
     return render(request, 'trainers.html', dict_trainers)
 
+@login_required
 def workouts(request):
     dict_workouts = {
         'workouts': Workout.objects.all()
     }
     return render(request, 'workouts.html', dict_workouts)
 
+@login_required
 def contact(request):
     return render(request, 'contact.html')
 
